37d76ea7ca9bf56eb2c0b2f5ed2e3249bf61f915
Deepak kushwaha <61049435+D804@users.noreply.github.com>
2024-04-04 10:07:17 +0530

    Assignment-I

--- block-BNaabe/node/server.js
+++ block-BNaabe/node/server.js
@@ -1,3 +1,4 @@
+/*
 let path = require('path');
 const queryString = require('querystring');
 let crFile = __filename;
@@ -5,7 +6,8 @@ let crFile = __filename;
 let absolutePath = path.join(__dirname, 'app.js');
 let relativePath = './index.html';
 let AbsolutePathModule = path.join(__dirname, 'index.html');
-
+*/
+/*
 let http = require('http');
 let server = http.createServer(handleServer);
 function handleServer(req, res) {
@@ -26,7 +28,8 @@ function handleServer(req, res) {
 server.listen(3000, () => {
   console.log('Running on port 3k');
 });
-
+*/
+/*
 let http = require('http');
 let crServer = http.createServer(serverHandler);
 
@@ -44,3 +47,97 @@ function serverHandler(req, res) {
 crServer.listen(9000, () => {
   console.log('Running on port 9k');
 });
+New Data
+let path = require('path');
+console.log(__filename);
+console.log(__dirname + '/app.js');
+console.log(__dirname, './index.html');
+let relative = path.join(__dirname, 'index.html');
+console.log(relative);
+*/
+/*
+let http = require('http');
+let qs = require('querystring');
+let server = http.createServer(handleServer);
+function handleServer(req, res) {
+  let store = '';
+  if (req.method === 'POST' && req.url === '/') {
+    req.on('data', (chunk) => {
+      store += chunk;
+    });
+    req.on('end', () => {
+      res.statusCode = 201;
+      let parsed = qs.parse(store);
+      res.end(JSON.stringify(parsed));
+    });
+  }
+}
+server.listen(3000, () => {
+  console.log('Requesting on localhost 3k');
+});
+*/
+/*
+let http = require('http');
+let qs = require('querystring');
+let server = http.createServer(handleServer);
+function handleServer(req, res) {
+  let store = '';
+  req.on('data', (chunk) => {
+    store += chunk;
+  });
+  req.on('end', () => {
+    if (req.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      let parsed = qs.parsed(store);
+      res.end(JSON.stringify(parsed));
+    }
+    if (req.headers['Content-Type'] === 'application/json') {
+      res.end(store);
+    }
+  });
+}
+server.listen(9000, () => {
+  console.log('Running on port 9k');
+});
+*/
+/*
+let http = require('http');
+let server = http.createServer(handleServer);
+function handleServer(req, res) {
+  let store = '';
+  req.on('data', (chunk) => {
+    store += chunk;
+  });
+  req.on('end', () => {
+
+    if (req.headers['Content-Type'] === 'application/json') {
+      let JsonData = JSON.parse(store);
+res.setHeader('Content-Type', 'text/html');
+res.end(`<h1>${JsonData.name}</h1><h2>${JsonData.email}</h2>`)
+    }
+  });
+}
+server.listen(9000, () => {
+  console.log('Running on port 9k');
+});
+*/
+/*
+let http = require('http');
+let qs = require('querystring');
+let server = http.createServer(handleServer);
+function handleServer(req, res) {
+  let store = '';
+  req.on('data', (chunk) => {
+    store += chunk;
+  });
+  req.on('end', () => {
+    if (req.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      let parsed = qs.parse(store);
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`<h1>${parsed.name}</h1><h2>${parsed.email}</h2>`);
+    }
+  });
+}
+server.listen(9000, () => {
+  console.log('Running on port 9k');
+});
+*/
