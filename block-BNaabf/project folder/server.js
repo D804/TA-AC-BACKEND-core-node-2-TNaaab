24f7e634babb1d9aa43e3ac9be73835e5bc403c7
Deepak kushwaha <61049435+D804@users.noreply.github.com>
2024-04-05 10:13:30 +0530

    Assignment -II

--- block-BNaabf/project folder/server.js
+++
@@ -1,6 +0,0 @@
-let http = require('http');
-let server = http.createServer(handleServer);
-function handleServer(req, res) {}
-server.listen(5000, () => {
-  console.log('Running on port 5k');
-});
--- block-BNaabf/projects/server/server.js
+++
@@ -1,3 +0,0 @@
-console.log('./index.js');
-console.log(__dirname);
-let http = require('http');
